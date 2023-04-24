fc1588a3f7deb08396ee1eb42ccfe1769fabf790
mriv <mr>
2023-04-24 00:47:42 -0400

    edits

--- system_setup/service_setup.sh
+++ system_setup/service_setup.sh
@@ -6,18 +6,6 @@ ROOT="/home/homer"
 mkdir -p "$ROOT/rbpi_monitor/data"
 mkdir -p "$ROOT/rbpi_monitor/error"
 
-# svc template 
-SVC_TEMPLATE="[Unit]
-Description=${svc_name}
-
-[Service]
-ExecStart=${svc_file}
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-"
-
 # populate  
 SVC_NAME=("RbpiHealth_Temp")
 SVC_SCRIPT=("monitor_temp")
@@ -30,28 +18,24 @@ for svc_name in $SVC_NAME; do
 
     # svc template 
     SVC_TEMPLATE="[Unit]
-    Description=${svc_name}
-
-    [Service]
-    ExecStart=${svc_script_path}
-    Restart=always
-
-    [Install]
-    WantedBy=multi-user.target
+    \nDescription=${svc_name}
+    \n[Service]
+    \nExecStart=${svc_script_path}
+    \nRestart=always
+    \n[Install]
+    \nWantedBy=multi-user.target
     "
-    echo $SVC_TEMPLATE
     # chmod .sh script 
-    chmod +x svc_script_path
+    chmod +x $svc_script_path
     
     # write service file 
-    sudo echo $SVC_TEMPLATE > "$PWD/test.txt"
-    #"/etc/systemd/system/${svc_script}.service"
+    sudo echo -e $SVC_TEMPLATE > "/etc/systemd/system/${svc_script}.service"
 
     # enable svc file 
-    #sudo "systemctl enable ${svc_script}.service"
+    sudo "systemctl enable ${svc_script}.service"
     
     # start svc 
-    #sudo "systemctl start ${svc_script}.service"
+    sudo "systemctl start ${svc_script}.service"
     
     i=$((i+1))
 
